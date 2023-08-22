da77066a871b1e3796092e602b795c4f14503852
José Eduardo <dudu.gois.123@gmail.com>
2023-08-22 20:27:36 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,96 @@
+import { listImgAndAudio } from "./list-img-and-audio.js"
+
+document.querySelectorAll('.image').forEach(element => {
+    element.addEventListener('mouseover', () => {
+        if (element.classList.contains(1)) {
+            element.src = listImgAndAudio[0].Gundyr2
+        }else if(element.classList.contains(2)){
+            element.src = listImgAndAudio[1].Namless2
+        }else if(element.classList.contains(3)){
+            element.src = listImgAndAudio[2].Sif2
+        }else if(element.classList.contains(4)){
+            element.src = listImgAndAudio[3].Soul_of_Cinder2
+        }else if(element.classList.contains(5)){
+            element.src = listImgAndAudio[4].Yhorm2
+        }else if(element.classList.contains(6)){
+            element.src = listImgAndAudio[5].Abyss2
+        }else if(element.classList.contains(7)){
+            element.src = listImgAndAudio[6].Ancient_Dragon2
+        }else if(element.classList.contains(8)){
+            element.src = listImgAndAudio[7].Four_Kings2
+        }else if(element.classList.contains(9)){
+            element.src = listImgAndAudio[8].Gael2
+        }else if(element.classList.contains(10)){
+            element.src = listImgAndAudio[9].Gwyn2
+        }else if(element.classList.contains(11)){
+            element.src = listImgAndAudio[10].Nito2
+        }else if(element.classList.contains(12)){
+            element.src = listImgAndAudio[11].Pontfice2
+        }else if(element.classList.contains(13)){
+            element.src = listImgAndAudio[12].Matheus2
+        }else if(element.classList.contains(14)){
+            element.src = listImgAndAudio[13].Dudu2
+        }
+    })
+    element.addEventListener('mouseout', () => {
+        if (element.classList.contains(1)) {
+            element.src = listImgAndAudio[0].Gundyr
+        }else if(element.classList.contains(2)){
+            element.src = listImgAndAudio[1].Namless
+        }else if(element.classList.contains(3)){
+            element.src = listImgAndAudio[2].Sif
+        }else if(element.classList.contains(4)){
+            element.src = listImgAndAudio[3].Soul_of_Cinder
+        }else if(element.classList.contains(5)){
+            element.src = listImgAndAudio[4].Yhorm
+        }else if(element.classList.contains(6)){
+            element.src = listImgAndAudio[5].Abyss
+        }else if(element.classList.contains(7)){
+            element.src = listImgAndAudio[6].Ancient_Dragon
+        }else if(element.classList.contains(8)){
+            element.src = listImgAndAudio[7].Four_Kings
+        }else if(element.classList.contains(9)){
+            element.src = listImgAndAudio[8].Gael
+        }else if(element.classList.contains(10)){
+            element.src = listImgAndAudio[9].Gwyn
+        }else if(element.classList.contains(11)){
+            element.src = listImgAndAudio[10].Nito
+        }else if(element.classList.contains(12)){
+            element.src = listImgAndAudio[11].Pontfice
+        }else if(element.classList.contains(13)){
+            element.src = listImgAndAudio[12].Matheus
+        }else if(element.classList.contains(14)){
+            element.src = listImgAndAudio[13].Dudu
+        }
+    })
+});
+
+document.querySelectorAll('.button').forEach(element => {
+    element.addEventListener('click', () => {
+        if(element.classList.contains(1)){
+            new Audio(listImgAndAudio[0].GundyrAudio).play()
+        }else if(element.classList.contains(2)){
+            new Audio(listImgAndAudio[1].NamlessAudio).play()
+        }else if(element.classList.contains(3)){
+            new Audio(listImgAndAudio[2].SifAudio).play()
+        }else if(element.classList.contains(4)){
+            new Audio(listImgAndAudio[3].Soul_of_CinderAudio).play()
+        }else if(element.classList.contains(5)){
+            new Audio(listImgAndAudio[4].YhormAudio).play()
+        }else if(element.classList.contains(6)){
+            new Audio(listImgAndAudio[5].AbyssAudio).play()
+        }else if(element.classList.contains(7)){
+            new Audio(listImgAndAudio[6].Ancient_DragonAudio).play()
+        }else if(element.classList.contains(8)){
+            new Audio(listImgAndAudio[7].Four_KingsAudio).play()
+        }else if(element.classList.contains(9)){
+            new Audio(listImgAndAudio[8].GaelAudio).play()
+        }else if(element.classList.contains(10)){
+            new Audio(listImgAndAudio[9].GwynAudio).play()
+        }else if(element.classList.contains(11)){
+            new Audio(listImgAndAudio[10].NitoAudio).play()
+        }else if(element.classList.contains(12)){
+            new Audio(listImgAndAudio[11].PontficeAudio).play()
+        }
+    })
+})
---
+++ src/js/list-img-and-audio.js
@@ -0,0 +1,70 @@
+export const listImgAndAudio = [
+    { 
+        Gundyr: "../imagens/gundao.jpg", 
+        Gundyr2: "../imagens/gundao2.jpg",
+        GundyrAudio: "../imagens/reis.wav"
+    },
+    { 
+        Namless: "../imagens/namless.jpg", 
+        Namless2: "../imagens/namless2.jpg",
+        NamlessAudio: "../imagens/namless.wav" 
+    },
+    { 
+        Sif: "../imagens/sif.jpg", 
+        Sif2: "../imagens/sif2.jpg",
+        SifAudio: "../imagens/lobao.wav" 
+    },
+    { 
+        Soul_of_Cinder: "../imagens/soulofcinder.jpg", 
+        Soul_of_Cinder2: "../imagens/soulofcinder2.jpg",
+        Soul_of_CinderAudio: "../imagens/cinder.wav" 
+    },
+    { 
+        Yhorm: "../imagens/yhorm.jpg", 
+        Yhorm2: "../imagens/yhorm2.jpg",
+        YhormAudio: "../imagens/yhorm.wav"
+    },
+    { 
+        Abyss: "../imagens/abyss.jpg", 
+        Abyss2: "../imagens/abyss2.jpg",
+        AbyssAudio: "../imagens/abyss.wav" 
+    },
+    { 
+        Ancient_Dragon: "../imagens/dragaoanciao.jpg", 
+        Ancient_Dragon2: "../imagens/dragaoanciao2.jpg",
+        Ancient_DragonAudio: "../imagens/dragon.wav" 
+    },
+    { 
+        Four_Kings: "../imagens/fourkings.jpg", 
+        Four_Kings2: "../imagens/fourkings2.jpg",
+        Four_KingsAudio: "../imagens/reis.wav"
+    },
+    { 
+        Gael: "../imagens/gael.jpg", 
+        Gael2: "../imagens/gael2.jpg",
+        GaelAudio: "../imagens/gael.wav" 
+    },
+    { 
+        Gwyn: "../imagens/gwyn.jpg", 
+        Gwyn2: "../imagens/gwyn2.jpg",
+        GwynAudio: "../imagens/gwyn.wav"
+    },
+    { 
+        Nito: "../imagens/nito.jpg", 
+        Nito2: "../imagens/nito2.jpg",
+        NitoAudio: "../imagens/nito.wav" 
+    },
+    { 
+        Pontfice: "../imagens/pontifice.jpg", 
+        Pontfice2: "../imagens/pontifice2.jpg",
+        PontficeAudio: "../imagens/sulivan.wav" 
+    },
+    { 
+        Matheus: "../imagens/matheus-foto.jpeg", 
+        Matheus2: "../imagens/matheus-foto.jpg" 
+    },
+    { 
+        Dudu: "../imagens/sensual.jpg", 
+        Dudu2: "../imagens/sensual2.jpg" 
+    }
+]
